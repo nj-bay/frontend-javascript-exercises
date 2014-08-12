592a721c5acbab7137f05771a3c1221ef8f17983
nj-bay <neal.jacob.415@gmail.com>
2014-08-12 15:45:22 -0700

    fixes string exercise to pass Mocha Tests

--- 04-strings/00-basics/challenge.js
+++ 04-strings/00-basics/challenge.js
@@ -1,19 +1,21 @@
-module.exports.formLetter = function(firstName, senderName, message) {
-
+module.exports.formLetter = function(London, Berlin, Yo) {
+     return "Hello " + London + ",\n\n" + Yo + "\n\nSincerely,\n" + Berlin;
 };
 
 module.exports.sliceItAndCombineIt = function(bigString, startA, endA, startB, endB) {
-
+    return bigString.substring(startA, endA) + bigString.substring(startB, endB);
 };
 
 module.exports.findFirstMatch = function(text, searchString) {
-
+    return text.indexOf(searchString);
 };
 
 module.exports.findLastMatch = function(text, searchString) {
-
+    return text.lastIndexOf(searchString);
 };
 
 module.exports.substringBetweenMatches = function(text, searchString) {
-
+    firstMatchPosition = text.indexOf(searchString) + searchString.length;
+  lastMatchPosition = text.lastIndexOf(searchString);
+  return text.substring(firstMatchPosition, lastMatchPosition);
 };
