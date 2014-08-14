6c333a89116ea88e8f097db2b96478663ed0afbf
nj-bay <neal.jacob.415@gmail.com>
2014-08-14 10:30:24 -0700

    completes Boolean Challenge

--- 05-booleans/00-basics/challenge.js
+++ 05-booleans/00-basics/challenge.js
@@ -1,19 +1,20 @@
-module.exports.equalStrings = function(stringOne, stringTwo) {
-
+module.exports.equalStrings = function(fun1, fun2) {
+    return fun1 == fun2;
 };
 
 module.exports.notEqual = function(one, two) {
-
+    return one !== two;
 };
 
-module.exports.inBetween = function(lower, middle, upper) {
-
+module.exports.inBetween = function(one, three, five) {
+    return one < three && three < five;
 };
 
 module.exports.outsideRanges = function(number) {
-
+    var num = (10 <= number && number <= 20) || (42 < number && number <= 75) || (1 < number && number < 6);
+    return !num;
 };
 
 module.exports.nameAndPrice = function(name, price) {
-
+    return (name === "NYTimes" || name === "LATimes") && (price >= 1);
 };
