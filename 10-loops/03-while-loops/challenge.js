37c360dfef4869289fddb09db447418c481ebefa
nj-bay <neal.jacob.415@gmail.com>
2014-10-06 22:29:09 -0700

    passing While loops

--- 10-loops/03-while-loops/challenge.js
+++ 10-loops/03-while-loops/challenge.js
@@ -1,4 +1,17 @@
-module.exports.stream = undefined;
+module.exports.stream = function (conditionalFn, actionFn) {
+    while (conditionalFn()) {
+        actionFn();
+    }
+};
 
-module.exports.sumNumbers = undefined;
+module.exports.sumNumbers = function (elems) {
+    var i = 0;
+    var accumulator = 0;
+    while (i < elems.length) {
+        accumulator += elems[i];
+        i++;
+    }
+
+    return accumulator;
+};
 
